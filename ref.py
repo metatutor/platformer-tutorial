3f14ef380492aeb61a8b2f50f27fd70591c92adf
eddy <edward@themetatutor.com>
2020-10-27 19:35:40 -0400

    updated ref.py organization

--- ref.py
+++ ref.py
@@ -11,13 +11,22 @@ win = pygame.display.set_mode((win_width,win_height))
 pygame.display.set_caption(caption)
 
 while True:
-    #length of time between each refresh
-    pygame.time.delay(25)
 
-    #event loop
+    #event block
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
+
+    #update block
+
+
+
+
+    #draw block
     win.fill(bg_color)
+
+    
+    #pygame updates
     pygame.display.update()
+    pygame.time.delay(25)
